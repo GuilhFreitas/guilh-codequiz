124fc61f85328c0a460595389cb33480236a000e
GuillhFreitas <guilherme.antraco@gmail.com>
2023-01-13 15:14:53 +0000

    startQuiz is now an anonymous function in the start button event listener. Started quiz function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,10 @@
 let timerEl = document.querySelector("#time");
 let startButtonEl = document.querySelector("#start");
 let startScreenEl =  document.querySelector("#start-screen");
+let quizEl = document.querySelector("#questions");
+let questionEl = document.querySelector("#question-title");
+let choicesEl = document.querySelector("#choices");
+
 
 let timeLeft = 75;
 
@@ -8,9 +12,35 @@ let timeLeft = 75;
 startScreenEl.addEventListener("click", function(event){
     if (event.target.matches("#start")){
         startScreenEl.setAttribute("class", "hide");
-
+        quizEl.removeAttribute("class", "hide");
+        quiz();
     }
 });
+
+function quiz(){
+    let questionNumber = 0;
+    questionEl.textContent = questionList[questionNumber].q;
+    let listEl = document.createElement("ol");
+    choicesEl.appendChild(listEl);
+    let but1 = document.createElement("button");
+    let but2 = document.createElement("button");
+    let but3 = document.createElement("button");
+    let but4 = document.createElement("button");
+    but1.textContent = "1. " + questionList[questionNumber].a1;
+    but2.textContent = "2. " + questionList[questionNumber].a2;
+    but3.textContent = "3. " + questionList[questionNumber].a3;
+    but4.textContent = "4. " + questionList[questionNumber].a4;
+    listEl.appendChild(but1);
+    listEl.appendChild(but2);
+    listEl.appendChild(but3);
+    listEl.appendChild(but4);
+
+    
+};
+
+function answerButton(){
+    
+}
 
 // starts timer on the top right of screen
 function countdown(){
