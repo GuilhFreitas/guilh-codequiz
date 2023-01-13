7125d2591d2684fe743ad7221d49301c4004f126
GuillhFreitas <guilherme.antraco@gmail.com>
2023-01-13 14:15:31 +0000

    added quiz questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,43 @@
+let questionList = [
+    {
+        q: "Commonly used data types DO NOT include:",
+        a1: "strings",
+        a2: "booleans",
+        a3: "alerts",
+        a4: "numbers",
+        correct: "a3"
+    },
+    {
+        q: "The condition in an if / else statement is enclosed within _____.",
+        a1: "quotes",
+        a2: "curly brackets",
+        a3: "parentheses",
+        a4: "square brackets",
+        correct: "a3"
+    },
+    {
+        q: "Arrays in Javascript can be used to store ______.",
+        a1: "numbers and strings",
+        a2: "other arrays",
+        a3: "booleans",
+        a4: "all of the above",
+        correct: "a4"
+    },           
+    {
+        q: "String values must be enclosed within ______ when being assigned to variables.",
+        a1: "commas",
+        a2: "curly brackets",
+        a3: "quotes",
+        a4: "parentheses",
+        correct: "a3"
+    },
+    {
+        q: "A very useful tool during development and debugging for printing content to the debugger is:",
+        a1: "Javascript",
+        a2: "terminal / bash",
+        a3: "for loops",
+        a4: "console log",
+        correct: "a4"
+    }              
+];
 
