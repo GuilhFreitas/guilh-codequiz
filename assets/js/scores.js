3bf24884c649d72b3f9e2c0efaff82c0705ad7de
GuillhFreitas <guilherme.antraco@gmail.com>
2023-01-17 22:07:11 +0000

    clear button in highscore page now clears scores from local storage and the display

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,14 +1,29 @@
 let highscoresEl = document.querySelector("#highscores");
 let clearEl = document.querySelector("#clear");
 
-// displays highscore list
-let highscores = JSON.parse(localStorage.getItem("quizScores"));
+scoreDisplay();
 
-for (i = 0; i < highscores.length; i++){
-    let score = highscores[i];
-    let liEl = document.createElement("li");
-    liEl.textContent = score[0] + " - " + score[1];
-    highscoresEl.appendChild(liEl);
+// displays highscore list
+function scoreDisplay(){
+    let highscores = JSON.parse(localStorage.getItem("quizScores"));
+    highscoresEl.innerHTML = "";
+
+    if (highscores !== null){
+        for (i = 0; i < highscores.length; i++){
+            let score = highscores[i];
+            let liEl = document.createElement("li");
+            liEl.textContent = score[0] + " - " + score[1];
+            highscoresEl.appendChild(liEl);
+        }
+    }else{
+        return
+    }
+
 }
 
+
 // clears highscore data
+clearEl.addEventListener("click", function(){
+    localStorage.removeItem("quizScores");
+    scoreDisplay();
+})
