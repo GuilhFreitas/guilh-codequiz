417abe43a1be2dd6f0e77564788fe1aaacb5eeca
GuillhFreitas <guilherme.antraco@gmail.com>
2023-01-17 14:44:54 +0000

    added score variable that increments on correct answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,9 +5,13 @@ let quizEl = document.querySelector("#questions");
 let questionEl = document.querySelector("#question-title");
 let choicesEl = document.querySelector("#choices");
 let feedbackEl = document.querySelector("#feedback");
+let endEl = document.querySelector("#end-screen");
+let scoreEl = document.querySelector("#final-score");
+let initialEl = document.querySelector("#initials");
 
 let questionNumber = 0;
 let timeLeft = 75;
+let score = 0;
 
 // starts the quiz when the button is clicked
 startScreenEl.addEventListener("click", function(event){
@@ -22,36 +26,39 @@ startScreenEl.addEventListener("click", function(event){
 // displays the next question
 function nextQuestion(){
 
-    choicesEl.innerHTML = "";
-    questionEl.textContent = questionList[questionNumber].q;
-
-    let listEl = document.createElement("ol");
-    choicesEl.appendChild(listEl);
-    listEl.addEventListener("click", answerButton);
-
-    let but1 = document.createElement("button");
-    let but2 = document.createElement("button");
-    let but3 = document.createElement("button");
-    let but4 = document.createElement("button");
-
-    but1.textContent = "1. " + questionList[questionNumber].a1;
-    but1.setAttribute("data-index", "1");
-    but2.textContent = "2. " + questionList[questionNumber].a2;
-    but2.setAttribute("data-index", "2");
-    but3.textContent = "3. " + questionList[questionNumber].a3;
-    but3.setAttribute("data-index", "3");
-    but4.textContent = "4. " + questionList[questionNumber].a4;
-    but4.setAttribute("data-index", "4");
-
-    listEl.appendChild(but1);
-    listEl.appendChild(but2);
-    listEl.appendChild(but3);
-    listEl.appendChild(but4);
-
+    if (questionNumber > questionList.length){
+        finalScore();
+    }else{
+        choicesEl.innerHTML = "";
+        questionEl.textContent = questionList[questionNumber].q;
+    
+        let listEl = document.createElement("ol");
+        choicesEl.appendChild(listEl);
+        listEl.addEventListener("click", answerButton);
+    
+        let but1 = document.createElement("button");
+        let but2 = document.createElement("button");
+        let but3 = document.createElement("button");
+        let but4 = document.createElement("button");
+    
+        but1.textContent = "1. " + questionList[questionNumber].a1;
+        but1.setAttribute("data-index", "1");
+        but2.textContent = "2. " + questionList[questionNumber].a2;
+        but2.setAttribute("data-index", "2");
+        but3.textContent = "3. " + questionList[questionNumber].a3;
+        but3.setAttribute("data-index", "3");
+        but4.textContent = "4. " + questionList[questionNumber].a4;
+        but4.setAttribute("data-index", "4");
+    
+        listEl.appendChild(but1);
+        listEl.appendChild(but2);
+        listEl.appendChild(but3);
+        listEl.appendChild(but4);
+    }
     
 };
 
-// function runs when an answer is clicked, evaluates answer, displays feedback and runs nextQuestion
+// runs when an answer is clicked, evaluates answer, displays feedback and runs nextQuestion
 function answerButton(event){
 
     if (event.target.matches("button")){
@@ -64,10 +71,12 @@ function answerButton(event){
             feedbackEl.textContent = "Wrong!";
         }else{
             feedbackEl.textContent = "Correct!";
+            score++;
         }
 
         feedbackEl.removeAttribute("class", "hide");
 
+        // hides feedback after 1 second
         let shortDisplay = setInterval(function() {
             
             feedbackEl.setAttribute("class", "hide");
@@ -78,7 +87,7 @@ function answerButton(event){
     }
 }
 
-// starts timer on the top right of screen
+// starts timer on the top right of screen, ends at 0
 function countdown(){
 
     let timeInterval = setInterval(function() {
